354073b874de39501b577da37879ab03d946eade
Jefry Pardo <jeffry.pardo01@unicatolica.edu.co>
2024-04-11 19:03:23 -0500

    version 1

--- src/app/module/cotizacion/components/pdf/pdf.component.ts
+++ src/app/module/cotizacion/components/pdf/pdf.component.ts
@@ -3,7 +3,6 @@ import { ResumenModel } from 'src/app/models/model/resumen.model';
 
 //@ts-ignore
 import jsPDF from 'jspdf';
-import html2canvas from 'html2canvas';
 import { ProductoModel } from 'src/app/models/model/producto.model';
 import { PrincipalService } from 'src/app/module/principal.service';
 import { CotizacionesService } from 'src/app/service/cotizaciones.service';
@@ -86,33 +85,6 @@ export class PdfComponent implements OnInit {
     const id_cotizacion = this.resumen.cotizacion.id;
     if(id_cotizacion == undefined || id_cotizacion == null) return;
     this.numero_cotizacion = id_cotizacion.split('-')[0];
-  }
-
-  downloadPDF() {
-    
-    const DATA = document.getElementById('htmlData');
-    const doc = new jsPDF('p', 'pt', 'a4');
-    
-    const options = {
-      background: 'white',
-      scale: 3
-    };
-
-    if(DATA == null) return;
-
-    html2canvas(DATA, options).then((canvas) => {
-
-      const img = canvas.toDataURL('image/PNG');
-      const bufferX = 15;
-      const bufferY = 15;
-      const imgProps = (doc as any).getImageProperties(img);
-      const pdfWidth = doc.internal.pageSize.getWidth() - 2 * bufferX;
-      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-      doc.addImage(img, 'PNG', bufferX, bufferY, pdfWidth, pdfHeight, undefined, 'FAST');
-      return doc;
-    }).then((docResult) => {
-      docResult.save(`${new Date().toISOString()}_tutorial.pdf`);
-    });
   }
 
   obtenerFechaConFormato() {
